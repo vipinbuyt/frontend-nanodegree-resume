15479cce108b6a651a5a6308f41ba1d15934d035
Vipin Bahuguna <vipin@buyt.in>
2017-10-03 19:28:50 +0530

    first variable

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+var awesomeThoughts = "I am Vipin and I am AWESOME!";
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+$("body").append(funThoughts);
+console.log(funThoughts);/
